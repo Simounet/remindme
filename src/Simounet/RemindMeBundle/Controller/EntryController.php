acbece7d7a68eb41e3d76ba1256fff571a673531
Simounet <contact@simounet.net>
2016-12-07 23:39:06 +0100

    Renamed Actions -> Entry

--- src/Simounet/RemindMeBundle/Controller/EntryController.php
+++ src/Simounet/RemindMeBundle/Controller/EntryController.php
@@ -9,14 +9,14 @@ use Simounet\RemindMeBundle\Entity\Item;
 use Simounet\RemindMeBundle\Entity\Entry,
     Simounet\RemindMeBundle\Form\EntryType;
 
-class ActionsController extends Controller
+class EntryController extends Controller
 {
 
     public function addAction(Request $request)
     {
         $entry = new Entry();
         $form = $this->createForm(EntryType::class, $entry, array(
-            'action' => $this->generateUrl('simounet_remindme_actions_add')
+            'action' => $this->generateUrl('simounet_remindme_entry_add')
         ));
 
         $form->handleRequest($request);
@@ -30,7 +30,7 @@ class ActionsController extends Controller
                 ->add('success', 'Saved');
             return $this->redirect($this->generateUrl('simounet_remindme_homepage'));
         }
-        return $this->render('SimounetRemindMeBundle:Actions:add.html.twig', array(
+        return $this->render('SimounetRemindMeBundle:Entry:add.html.twig', array(
             'form' => $form->createView()
         ));
     }
